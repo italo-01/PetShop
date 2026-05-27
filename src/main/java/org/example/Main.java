ea3c71248b6afe2cd1e32ea6079429d7d37a4338
ITALO COSTA RIBEIRO <italocosta019641@gmail.com>
2026-05-26 22:02:31 -0300

    cria cases

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,10 @@ package org.example;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    private final static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         var scanner = new Scanner(System.in);
@@ -16,14 +17,24 @@ public class Main {
             System.out.println("2 - Abastecer a maquina com água");
             System.out.println("3 - Abastecer a maquina com shampoo");
             System.out.println("4 - Verifica água da máquina");
-            System.out.println("5 - verifica Shampoo da máquina");
+            System.out.println("5 - verifica Shampoo da máquina")
             System.out.println("6- Verificar se tem pet no banho");
             System.out.println("7 - Colocar pet na maquina ");
             System.out.println("8- Retirar pet da máquina");
             System.out.println("9 - Limpar a máquina");
             System.out.println("0 - Sair");
             option = scanner.nextInt();
+
+            switch (option){
+                case 7 ->setPetinPetMachine();
+            }
+
         }while (option != 0);
 
+    }
+
+    public static void setPetinPetMachine(){
+        System.out.println("Informe o nome do pet");
+        var name = s
     }
 }
