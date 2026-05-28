63bd98b1d79a12f88bfabe652db5bb8abf941ba4
ITALO COSTA RIBEIRO <italocosta019641@gmail.com>
2026-05-27 23:23:26 -0300

    Cria case 1 ao 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -27,26 +27,54 @@ public class Main {
             option = scanner.nextInt();
 
             switch (option){
-                case 5 ->
+                case 1 -> petMachine.takeAShower();
+                case 2 -> setWater();
+                case 3 -> setShampoo();
+                case 4 -> verifyWater();
+                case 5 -> verifyShampoo();
                 case 6 -> checkIfHasPetInMachine();
                 case 7 ->setPetinPetMachine();
                 case 8 -> petMachine.removePet();
                 case 9 -> petMachine.wash();
+                case 0 -> System.exit();
+                default -> System.out.println("Opção invalida");
             }
 
         }while (option != 0);
 
     }
 
+    private static void setShampoo(){
+        System.out.println("Tentando colocar agua na maquina");
+        petMachine.addShampoo();
+
+    }
+
+    private static void setWater(){
+        System.out.println("Tentando colocar agua na maquina");
+        petMachine.addWater();
+
+    }
+
+    private static void verifyWater() {
+        var amount = petMachine.getShampoo();
+        System.out.println("A máquina está no momento com " + amount + " litro(s) de shampoo");
+    }
+
+    private static void verifyShampoo() {
+        var amount = petMachine.getShampoo();
+        System.out.println("A máquina está no momento com " + amount + " litro(s) de shampoo");
+    }
+
     private static void checkIfHasPetInMachine() {
-        var hasPet = petMachine.hasPet();
+        var haspet = petMachine.hasPet();
         petMachine.hasPet();
-        System.out.println(hasPet? "Tem pet na maquina" : "Não tem pet na máquina");
+        System.out.println(haspet? "Tem pet na maquina" : "Não tem pet na máquina");
     }
 
     public static void setPetinPetMachine(){
         var name = "";
-        while (name.isEmpty() || name.isEmpty()) {
+        while (name == null || name.isEmpty()) {
             System.out.println("Informe o nome do pet");
             name = scanner.nextLine();
         }
